9fbac1ff6bc882c091c7957afbadf272808bc398
calvincxz <cxingzhu@hotmail.com>
2019-08-17 22:39:44 +0800

    level 2

--- duke-master/src/main/java/Duke.java
+++ duke-master/src/main/java/Duke.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
-
+        ArrayList<String> list = new ArrayList<>();
         System.out.println("Hello! I'm Duke" + "\n" + "What can I do for you?");
 
         Scanner sc = new Scanner(System.in);
@@ -10,8 +11,13 @@ public class Duke {
             String input = sc.nextLine();
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
-            } else {
-                System.out.println(input);
+            } else if(input.equals("list")) {
+                for (int i = 1; i <= list.size(); i++) {
+                    System.out.println( i + ". " + list.get(i-1));
+                }
+            } else  {
+                list.add(input);
+                System.out.println("added: " + input);
             }
         }
     }
